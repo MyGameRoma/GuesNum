87e55652253e0f61c03e1e41dceaf9cda364b0f7
roman <roman.mihailov9@gmail.com>
2019-10-01 19:41:44 +0300

    yes or no

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.sun.security.jgss.GSSUtil;
 
 import javax.sound.midi.Soundbank;
+import javax.swing.*;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,33 +14,61 @@ public class Main {
 
     public static void main(String[] args) {
         String answer;
-            do {
+        do {
 
-                int myNum = rand.nextInt(100) + 1;
-                System.out.println(myNum);
-                boolean userwon = false;
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println(myNum);
+            boolean userwon = false;
 
-                for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {
+                int userNum = askint("Enter you number from 1 to 100", 1, 100);
 
-                    System.out.println("Enter you number from 1 to 100");
 
-                    int userNum = scan.nextInt();
-                    if (userNum == myNum) {
-                        System.out.println("molodec");
-                        userwon = true;
-                        break;
+                if (userNum == myNum) {
+                    System.out.println("molodec");
+                    userwon = true;
+                    break;
 
-                    } else if (userNum > myNum) {
-                        System.out.println("NEt chutj menjshe");
-                    } else if (userNum < myNum) {
-                        System.out.println("Net chutj boljshe");
-                    }
+                } else if (userNum > myNum) {
+                    System.out.println("NEt chutj menjshe");
+                } else if (userNum < myNum) {
+                    System.out.println("Net chutj boljshe");
                 }
-                if (!userwon) {
-                    System.out.println("loozer");
-                }
-                System.out.println("Do you ready for next round? yes or no");
-                answer = scan.next();
-            } while (answer.equalsIgnoreCase("yes"));
+            }
+            if (!userwon) {
+                System.out.println("loozer");
+            }
+
+        } while (askWord("Do you ready for next round? yes or no"));
+
+        System.out.println("goodbaiii");
+    }
+
+    static int askint(String msg, int min, int max) {
+        while (true) {
+            System.out.println(msg);
+            int answer = scan.nextInt();
+            if (answer >= min && answer <= max) {
+                return answer;
+            }
+            System.out.printf("please enter currekt from %d to %d\n", min, max);
+
+
+        }
+
+    }
+
+    static boolean askWord (String msg) {
+        while (true) {
+            System.out.println(msg);
+            String answer = scan.next();
+            boolean isy = answer.equalsIgnoreCase("yes");
+            boolean isn = answer.equalsIgnoreCase("no");
+
+            if (isy || isn) {
+                return isy;
+            }
+            System.out.println("Write yes or no");
+        }
     }
 }
